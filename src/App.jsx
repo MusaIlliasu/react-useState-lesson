b5dfc83465cf2b95375bbe2a8248ce28199c83e5
mi <musailliasu7@gmail.com>
2024-04-30 08:59:55 +0100

    implemented session

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,42 @@ import Users from "./pages/Users";
 import AddUser from "./pages/AddUser";
 
 const App = () => {
-    const [users, setUsers] = useState([]);
+    const [users, setUsers] = useState(() => {
+        const items = localStorage.getItem("items");
+        if(items){ return JSON.parse(items); }
+        return [];
+    });
     const [showAdduser, setShowAddUser] = useState(false);
 
+    const handleAddUser = (user) => {
+        const items = localStorage.getItem("items");
+
+        if(items){
+            const updatedItems = [user, ...JSON.parse(items)];
+            localStorage.setItem("items", JSON.stringify(updatedItems));
+            setUsers(updatedItems);
+            return;
+        }
+
+        localStorage.setItem("items", JSON.stringify([user]));
+        setUsers([user]);
+    }
+
     const handleRemoveUser = (userId) => {
+        const items = localStorage.getItem("items");
+        if(items){
+            const parsedItems = JSON.parse(items);
+            const updatedItems = parsedItems.filter(user => user.id !== userId);
+            localStorage.setItem("items", JSON.stringify(updatedItems));
+            setUsers(updatedItems);
+            return;
+        }
+
         return setUsers(users.filter(user => user.id !== userId));
     }
 
     const handleUserUpdate = (updatedUser) => {
+        
         const updatedUsers = users.map(user => {
             if(user.id === updatedUser.id){ return updatedUser }
             return user;
@@ -26,7 +54,7 @@ const App = () => {
 
             <AddUser
                 show={showAdduser}
-                setParentUser={(user) => setUsers([user, ...users])}
+                setParentUser={(user) => handleAddUser(user)}
                 close={() => setShowAddUser(false)}
             />
 
